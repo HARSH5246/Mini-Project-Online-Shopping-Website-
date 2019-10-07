37d0bc1dd752329c9adc156086a4fee2a56e2c28
Harsh Jain <harsh.jain_cs17@gla.ac.in>
2019-10-07 10:55:08 +0530

    sample

---
+++ sample.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+
+<body>
+    <script>
+
+
+    </script>
+    <?php
+
+       //$name = $_POST['brand'];
+        //echo $name;
+        //$name =  $_POST['brand'];
+         echo '<script language="javascript">';
+echo 'alert("message successfully sent")';
+echo '</script>';
+        ?>
+</body>
+</html>
